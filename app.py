e3ba493eb1a9ea808f364a0ef47b0ef115e6e1ba
Panvi Krishna <krishp1529@gmail.com>
2025-02-14 22:35:32 +0530

    Updated app.py to use Waitress for production

--- app.py
+++ app.py
@@ -80,5 +80,7 @@ def decrypt():
 def download_file(filename):
     return send_file(os.path.join("static", "uploads", filename), as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+
